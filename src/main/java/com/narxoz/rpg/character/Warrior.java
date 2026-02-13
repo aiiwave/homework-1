933196a738935341e3ad1f9a55cf093b45834edb
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-13 13:19:19 +0500

    aii

--- src/main/java/com/narxoz/rpg/character/Warrior.java
+++ src/main/java/com/narxoz/rpg/character/Warrior.java
@@ -1,56 +1,49 @@
 package com.narxoz.rpg.character;
 
-public interface Weapon{
-    String getName();
-    int getDamage();
-}
+import com.narxoz.rpg.equipment.Weapon;
+import com.narxoz.rpg.equipment.Armor;
 
-
-public class Warrior implements Character {
-
+public class Warrior extends Character {
     private String name;
     private int health;
     private int mana;
     private int strength;
     private int intelligence;
 
-    // TODO: Add fields for equipped weapon and armor
-    // Think: Should Warrior know about its equipment?
-
-
-    public Warrior(String name) {
+    public Warrior(String name){
+        super(name);
         this.name = name;
-        // Warrior stats: high health and strength, low mana and intelligence
-        this.health = 150;
-        this.mana = 30;
-        this.strength = 80;
-        this.intelligence = 20;
+        this.health = 180;
+        this.mana = 20;
+        this.strength = 90;
+        this.intelligence = 10;
     }
-
-    // TODO: Implement methods from Character interface
-    // You need to define those methods in Character interface first!
-
-    // Example method structure:
+    
+    @Override
     public String getName() {
-        return name;
+        return this.name;
     }
 
+    @Override
     public void displayStats() {
-        System.out.println("=== " + name + " (Warrior) ===");
-        System.out.println("Health: " + health);
-        System.out.println("Mana: " + mana);
-        System.out.println("Strength: " + strength);
-        System.out.println("Intelligence: " + intelligence);
-    }
+        System.out.println("=== " + this.name + " (Warrior) ===");
+        System.out.println("Health: " + this.health);
+        System.out.println("Mana: " + this.mana);
+        System.out.println("Strength: " + this.strength);
+        System.out.println("Intelligence: " + this.intelligence);
 
+    String wName = (weapon != null) ? weapon.getName() : "No Weapon";
+    String aName = (armor != null) ? armor.getName() : "No Armor";
+    System.out.println("Equipment: " + wName + ", " + aName);}
+
+    @Override
     public void useSpecialAbility() {
-        System.out.println(name + " uses BERSERKER RAGE! Strength temporarily increased!");
+        System.out.println(name + " uses 'POWER OF THE GODS'! He is now stronger!");
     }
+    public void equipWeapon(Weapon weapon){
+        this.weapon = weapon;
+        System.out.println(this.name + " takes the " + weapon.getName());}
 
-    // TODO: Add equipment-related methods
-    // Examples:
-    // - void equipWeapon(Weapon weapon)
-    // - void equipArmor(Armor armor)
-    // - void displayEquipment()
-
-}
+    public void equipArmor(Armor armor){
+        this.armor = armor;
+        System.out.println(this.name + " puts on the " + armor.getName());}}
