cfb23da986730b34cb838c8c56f1ea1ef4c99c8c
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-14 02:51:55 +0500

    aii

--- src/main/java/com/narxoz/rpg/factory/CharacterFactory.java
+++ src/main/java/com/narxoz/rpg/factory/CharacterFactory.java
@@ -1,1 +1,14 @@
+package com.narxoz.rpg.factory;
+import com.narxoz.rpg.character.*;
 
+public class CharacterFactory{
+	public Character createCharacter(String type, String name){
+		if(type.equalsIgnoreCase("WARRIOR")){
+			return new Warrior(name);}
+		else if(type.equalsIgnoreCase("MAGE")){
+			return new Mage(name);}
+		else if(type.equalsIgnoreCase("ARCHER")){
+			return new Archer(name);}
+		return null;
+	}
+}
