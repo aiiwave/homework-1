b432cdde0fd5e2401d23db362307469d1d4d5b95
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-13 13:42:39 +0500

    aii

--- src/main/java/com/narxoz/rpg/character/Warrior.java
+++ src/main/java/com/narxoz/rpg/character/Warrior.java
@@ -4,26 +4,18 @@ import com.narxoz.rpg.equipment.Weapon;
 import com.narxoz.rpg.equipment.Armor;
 
 public class Warrior extends Character {
-    private String name;
-    private int health;
-    private int mana;
-    private int strength;
-    private int intelligence;
-
     public Warrior(String name){
         super(name);
-        this.name = name;
         this.health = 180;
         this.mana = 20;
         this.strength = 90;
         this.intelligence = 10;
     }
-    
     @Override
-    public String getName() {
-        return this.name;
+    public String getCharacterClass(){
+        return "Warrior";
     }
-
+    
     @Override
     public void displayStats() {
         System.out.println("=== " + this.name + " (Warrior) ===");
@@ -32,18 +24,18 @@ public class Warrior extends Character {
         System.out.println("Strength: " + this.strength);
         System.out.println("Intelligence: " + this.intelligence);
 
-    String wName = (weapon != null) ? weapon.getName() : "No Weapon";
-    String aName = (armor != null) ? armor.getName() : "No Armor";
-    System.out.println("Equipment: " + wName + ", " + aName);}
+        String wName = (weapon != null) ? weapon.getName() : "No Weapon";
+        String aName = (armor != null) ? armor.getName() : "No Armor";
+        System.out.println("Equipment: " + wName + ", " + aName);}
 
     @Override
-    public void useSpecialAbility() {
-        System.out.println(name + " uses 'POWER OF THE GODS'! He is now stronger!");
+    public String useSpecialAbility() {
+        return name + " uses 'POWER OF THE GODS'! He is now stronger!";
     }
     public void equipWeapon(Weapon weapon){
-        this.weapon = weapon;
+        setWeapon(weapon);
         System.out.println(this.name + " takes the " + weapon.getName());}
 
     public void equipArmor(Armor armor){
-        this.armor = armor;
+        setArmor(armor);
         System.out.println(this.name + " puts on the " + armor.getName());}}
