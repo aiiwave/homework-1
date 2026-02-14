49bc6d32d9def97a606bcdc327dfe75c3e95d47c
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-14 09:10:56 +0500

    Refactor Character class for improved readability

--- src/main/java/com/narxoz/rpg/character/Character.java
+++ src/main/java/com/narxoz/rpg/character/Character.java
@@ -3,28 +3,18 @@ package com.narxoz.rpg.character;
 import com.narxoz.rpg.equipment.Weapon;
 import com.narxoz.rpg.equipment.Armor;
 
-public abstract class Character{
+public abstract class Character {
     protected String name;
-    protected int health;
-    protected int mana;
-    protected int strength;
-    protected int intelligence;
+    protected int health, mana, strength, intelligence;
     protected Weapon weapon;
     protected Armor armor;
 
-    public Character(String name){
-        this.name = name;
-    }
+    public Character(String name) { this.name = name; }
+
+    public void setWeapon(Weapon weapon) { this.weapon = weapon; }
+    public void setArmor(Armor armor) { this.armor = armor; }
+
+    public abstract void displayStats();
     public abstract String useSpecialAbility();
     public abstract String getCharacterClass();
-    public abstract void displayStats();
-
-    public void setWeapon(Weapon weapon){
-        this.weapon = weapon;
-        System.out.println(this.name + " takes the " + weapon.getName());
-    }
-    public void setArmor(Armor armor){
-        this.armor = armor;
-        System.out.println(this.name + " puts on the " + armor.getName());
-    }}
-
+}
