3e751d9bad192cc48cb868ccc4f26cb53fe8262f
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-14 06:20:12 +0500

    aii

--- src/main/java/com/narxoz/rpg/equipment/IronSword.java
+++ src/main/java/com/narxoz/rpg/equipment/IronSword.java
@@ -1,24 +1,5 @@
 package com.narxoz.rpg.equipment;
 
-public class IronSword implements Weapon{
-    private String name;
-    private int damage;
-
+public class IronSword extends Weapon{
     public IronSword(){
-        this.name = "Iron Sword";
-        this.damage = 25;}
-
-    @Override
-    public String getName(){
-        return name;}
-    @Override
-    public int getDamageValue(){
-        return damage;}
-
-    @Override
-    public String getAttackAction(){
-        return "Swings a heavy iron blade";}
-
-    @Override
-    public String getDamageType() {
-        return "Physical";}}
+        super("Iron Sword", 25);}}
