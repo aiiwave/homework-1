9dd55fce91f81e6b09b23ea85044681ebb354f5f
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-14 08:46:02 +0500

    Update character instantiation and display in Main
    Refactored character creation and display logic in Main class.

--- src/main/java/com/narxoz/rpg/Main.java
+++ src/main/java/com/narxoz/rpg/Main.java
@@ -1,23 +1,23 @@
 package com.narxoz.rpg;
 
 import com.narxoz.rpg.character.Character;
-import com.narxoz.rpg.factory.CharacterFactory;
-import com.narxoz.rpg.factory.EquipmentFactory;
-import com.narxoz.rpg.equipment.Weapon;
-import com.narxoz.rpg.equipment.Armor;
+import com.narxoz.rpg.factory.factory.*;
+import com.narxoz.rpg.factory.equipment.*;
+
 
 public class Main {
     public static void main(String[] args) {
         CharacterFactory charFactory = new CharacterFactory();
         EquipmentFactory equipFactory = new EquipmentFactory();
 
-        Character myWarrior = charFactory.createCharacter("Warrior", "Aragorn");
+        Character hero = charFactory.createCharacter("Warrior", "Aragorn");
         Weapon sword = equipFactory.createWeapon("Sword");
         Armor plate = equipFactory.createArmor("Plate");
     
         if(myWarrior != null){
-            myWarrior.setWeapon(sword);
-            myWarrior.setArmor(plate);
-            myWarrior.displayStats();
-            System.out.println(myWarrior.useSpecialAbility());
+            hero.setWeapon(sword);
+            hero.setArmor(plate);
+            System.out.println("Class: " + hero.useSpecialAbility());
+            hero.displayStats();
+            System.out.println("Action: " + hero.useSpecialAbility());
         }}}
