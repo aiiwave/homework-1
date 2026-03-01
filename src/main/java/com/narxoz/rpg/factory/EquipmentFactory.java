4afd74e2fc77a03c60e583ad826de0ebe3914653
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-03-01 18:06:21 +0500

    Refactor EquipmentFactory to use interface

--- src/main/java/com/narxoz/rpg/factory/EquipmentFactory.java
+++ src/main/java/com/narxoz/rpg/factory/EquipmentFactory.java
@@ -1,12 +1,6 @@
 package com.narxoz.rpg.factory;
-
 import com.narxoz.rpg.equipment.*;
-
-public class EquipmentFactory{
-	public Weapon createWeapon(String type){
-		if(type.equalsIgnoreCase("SWORD")) return new IronSword();
-		return new Weapon("Common Weapon", 10);
-	}
-	public Armor createArmor(String type){
-		return new Armor("Wizard Robe", 10);
-	}}
+public interface EquipmentFactory {
+    Weapon createWeapon();
+    Armor createArmor();
+}
