de15b15da8589e35517b839be844ec25068ae559
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-03-01 18:05:07 +0500

    Refactor CharacterFactory to abstract class
    Refactored CharacterFactory to be abstract and simplified character creation method.

--- src/main/java/com/narxoz/rpg/factory/CharacterFactory.java
+++ src/main/java/com/narxoz/rpg/factory/CharacterFactory.java
@@ -1,14 +1,5 @@
 package com.narxoz.rpg.factory;
-import com.narxoz.rpg.character.*;
-
-public class CharacterFactory{
-	public com.narxoz.rpg.character.Character createCharacter(String type, String name){
-		if(type.equalsIgnoreCase("WARRIOR")){
-			return new Warrior(name);}
-		else if(type.equalsIgnoreCase("MAGE")){
-			return new Mage(name);}
-		else if(type.equalsIgnoreCase("ARCHER")){
-			return new Archer(name);}
-		return null;
-	}
+import com.narxoz.rpg.character.Character;
+public abstract class CharacterFactory {
+    public abstract Character createCharacter(String name);
 }
