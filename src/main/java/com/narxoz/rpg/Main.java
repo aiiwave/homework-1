3e75118c7cd2760f151c90c9a015da1769500f35
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-03-01 18:08:51 +0500

    Update Main.java

--- src/main/java/com/narxoz/rpg/Main.java
+++ src/main/java/com/narxoz/rpg/Main.java
@@ -1,23 +1,16 @@
 package com.narxoz.rpg;
-
 import com.narxoz.rpg.character.Character;
-import com.narxoz.rpg.factory.factory.*;
-import com.narxoz.rpg.factory.equipment.*;
-
+import com.narxoz.rpg.factory.*;
 
 public class Main {
     public static void main(String[] args) {
-        CharacterFactory charFactory = new CharacterFactory();
-        EquipmentFactory equipFactory = new EquipmentFactory();
-
-        Character hero = charFactory.createCharacter("Warrior", "Aragorn");
-        Weapon sword = equipFactory.createWeapon("Sword");
-        Armor plate = equipFactory.createArmor("Plate");
-    
-        if(myWarrior != null){
-            hero.setWeapon(sword);
-            hero.setArmor(plate);
-            System.out.println("Class: " + hero.useSpecialAbility());
-            hero.displayStats();
-            System.out.println("Action: " + hero.useSpecialAbility());
-        }}}
+        CharacterFactory factory = new WarriorFactory();
+        Character hero = factory.createCharacter("арагорн");
+        
+        EquipmentFactory eqFactory = new MedievalEquipmentFactory();
+        hero.setWeapon(eqFactory.createWeapon());
+        
+        hero.displayStats();
+        System.out.println(hero.useSpecialAbility());
+    }
+}
