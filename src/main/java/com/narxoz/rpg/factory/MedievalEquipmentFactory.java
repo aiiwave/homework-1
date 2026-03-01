0341353809bc423d7890930d40c9609df10caada
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-03-01 18:06:36 +0500

    Implement MedievalEquipmentFactory for equipment creation

---
+++ src/main/java/com/narxoz/rpg/factory/MedievalEquipmentFactory.java
@@ -0,0 +1,6 @@
+package com.narxoz.rpg.factory;
+import com.narxoz.rpg.equipment.*;
+public class MedievalEquipmentFactory implements EquipmentFactory {
+    @Override public Weapon createWeapon() { return new IronSword(); }
+    @Override public Armor createArmor() { return new PlateArmor(); }
+}
